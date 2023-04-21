2b7137ae1086fb7d981dea98fb260d3cbff4bd22
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-20 21:36:09 -0500

    refactor: Reroute Poems component to include portKey link

--- src/components/Poems/Poems.js
+++ src/components/Poems/Poems.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Poem from '../Poem/Poem';
 import './Poems.css';
 import { Link, Route } from 'react-router-dom';
 
 const Poems = ({poems}) => {
+  const [portKeyStyle, setPortKeyStyle] = useState({
+    width: '35px',
+    position: 'absolute',
+    left: '',
+    top: '',
+  });
+  const [portKeyIndex, setPortKeyIndex] = useState(2);
 
-  console.log(poems)
+  const keySrc = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzBmZDQ5ZDRkYzk3ZDcwYmI3YmUyMzEyNzE3ZDdhM2RlY2YzZTRjYiZlcD12MV9pbnRlcm5hbF9naWZzX2dpZklkJmN0PXM/LuvAW3OKmBgywTpGDY/giphy.gif';
+  const bodyHeight = document.body.clientHeight;
+  const bodyWidth = document.body.clientWidth;
+  
+  const shuffleKeyPosition = () => {
+    var randPosX = Math.floor((Math.random() * bodyWidth));
+    var randPosY = Math.floor((Math.random() * bodyHeight));
+    setPortKeyStyle({width: '35px', position: 'absolute', left:`${randPosX}px`, top:`${randPosY}px` })
+  }
+
+  const portKey = <img src={keySrc} style={portKeyStyle} onClick={shuffleKeyPosition}/>
+
   const poEms = poems.map((poem, index) => {
     return <Route exact path={`/poem/${index+1}`}><Poem poem={poem} key={index} id={index+1}/></Route>
-  })
+  });
   
   return (
     <>
+      {portKeyIndex < 4 ?
+        <Link to={`/poem/${portKeyIndex}`} onClick={()=> {setPortKeyIndex(portKeyIndex +1)}}>{portKey}</Link>
+      : <Link to='/'>{portKey}</Link>
+      }
       {poEms}
     </>
   )
