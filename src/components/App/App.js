e8d8bcdc783f52027f37a0e88936397bfb75437f
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-22 17:56:24 -0500

    fix: Add function to check for lists of poems less than three

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,6 @@ const App = () => {
   const [poets, setPoets] = useState([]);
   const [poet, setPoet] = useState('Emily Dickinson'); //errors were being thrown without a truthy initial value
   const [poems, setPoems] = useState([]);
-  const [proceed, setProceed] = useState(false);
 
 
   useEffect(() => {
@@ -25,23 +24,29 @@ const App = () => {
   useEffect(() => {
     getPoems(poet)
     .then(data => {
-      console.log(data)
-      const indices = getRandomIndex(data);
-      setPoems([data[indices[0]], data[indices[1]], data[indices[2]]])});
+      checkNumOfPoems(data)
+    })
   }, [poet]);
   
   const getRandomIndex = (data) => {
       let index1 = Math.floor(Math.random() * data.length);
       let index2 = Math.floor(Math.random() * data.length);
       let index3 = Math.floor(Math.random() * data.length);
-      if (data.length < 3){
-        return [0, 1, index1]
-      } else if (index1 !== index2 && index2 !== index3 && index1 !== index3) {
-        return [index1, index2, index3];
-      } else {
-        return getRandomIndex(data);
-      }
+      return [index1, index2, index3];
   };
+
+  const checkNumOfPoems = (data) => {
+    if(data.length === 1) {
+      setPoems([data[0]])
+    } else if ( data.length === 2) {
+      setPoems([data[0], data[1]])
+    } else if (data.length === 3) {
+      setPoems([data[0], data[1], data[2]])
+    } else {
+      const indices = getRandomIndex(data);
+      setPoems([data[indices[0]], data[indices[1]], data[indices[2]]])
+    }
+  }
 
   return (
     <div className="App">
