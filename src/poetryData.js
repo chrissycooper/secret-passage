1ef0b5ad447c7d6a81f22620f267373aa7719eb6
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-04-21 19:21:56 -0500

    refactor: Adjust data to be more inclusive

--- src/poetryData.js
+++ src/poetryData.js
@@ -1,9 +1,9 @@
 const commonWords = [
-  {type: 'time', words: ['time','life', 'years','long', 'day']},
-  {type: 'love', words:['love','light','air','white','heart']},
-  {type: 'spooky', words: ['night','dead','dark','left', 'world']},
-  {type: 'body', words: ['hand','body','face', 'eyes', 'man']},
-  {type: 'nature', words: ['sky','sun','wind','god','water']}
+  {type: 'time', words: ['time','life', 'years','long', 'day', 'Time', 'Life', 'Years','Long', 'Day']},
+  {type: 'love', words:['love','light','air','white','heart','Love','Light','Air','White','Heart']},
+  {type: 'spooky', words: ['night','dead','dark','left', 'world', 'Night','Dead', 'Dark', 'Left', 'World']},
+  {type: 'body', words: ['hand','body','face', 'eyes', 'man', 'Hand','Body','Face', 'Eyes', 'Man']},
+  {type: 'nature', words: ['sky','sun','wind','god','water', 'Sky','Sun','Wind','God','Water']}
 ]
 
 export default commonWords;
