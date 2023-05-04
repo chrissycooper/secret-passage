d09b252ce6218cd7302bc0f4363c69347024e710
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-05-03 22:10:35 -0500

    fix: Update description

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
           <div className='txt-background'>
             <h1 className='welcome'>Welcome to</h1>
             <h2 className='title'>Secret Passage</h2>
-            <h3 className='desc'>On the next page there is a list of poets. If you select a poet, up to   three of their works will be presented to you. Look for the hidden key <img className='port-key' src={keySrc} alt='the port key!'/> to move on to the next poem.</h3>
+            <h3 className='desc'>On the next page there is a list of poets. If you select a poet, up to   ten of their works will be presented to you. Look for the hidden key <img className='port-key' src={keySrc} alt='the port key!'/> to move on to the next poem.</h3>
             <h3 className='direction'>Click on the door below to begin!</h3>
             <Link className='arch-link'to='/select-poet'><img className='trapdoor'src={require('./tdoorwoodmed_gt.png')} alt='a trapdoor'/></Link>              
           </div>
