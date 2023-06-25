b30ec04119f2d01a42fa97bcc666957e70c3d0f8
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-06-25 15:14:56 -0500

    refactor: Reorganize utilities part two

---
+++ src/utilities/commonWords.js
@@ -0,0 +1,9 @@
+const commonWords = [
+  {type: 'time', words: ['time','life', 'years','long', 'day', 'Time', 'Life', 'Years','Long', 'Day']},
+  {type: 'love', words:['love','light','air','white','heart','Love','Light','Air','White','Heart']},
+  {type: 'spooky', words: ['night','dead','dark','left', 'world', 'Night','Dead', 'Dark', 'Left', 'World']},
+  {type: 'body', words: ['hand','body','face', 'eyes', 'man', 'Hand','Body','Face', 'Eyes', 'Man']},
+  {type: 'nature', words: ['sky','sun','wind','god','water', 'Sky','Sun','Wind','God','Water']}
+]
+
+export default commonWords;
