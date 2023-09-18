af227293e828a334b57400e7d3238191dd0a7a37
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-09-17 19:36:32 -0500

    Fix: Change select your poet form value from empty string to none

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -28,7 +28,7 @@ const Form = ({ poets, setPoet, poet }) => {
         <p className='form-text'> These categories were based on the occurance of one of the 25 most commonly used words in around 3,000 poems from <a href='https://www.poetryfoundation.org/poetrymagazine'>Poetry Magazine</a>. This analysis was done by Robert Peake. You can read more about it at <a href='https://www.robertpeake.com/archives/6676-top-poetry-words.html'>his website</a>. If the poem does not contain one of those words, it will have a default style.</p>
         <form className='submit-wrap'>
           <select className='form-select' name='authors' id='authors' onChange={handleChange}>
-            <option value='' defaultValue> Select Your Poet</option>
+            <option value='none' defaultValue> Select Your Poet</option>
             <option value='random'>random</option>
             {options}
           </select>
